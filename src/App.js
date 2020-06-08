f3dd25b653a625d3ad5c72b2e35450c3a8ed4d84
luongdoan277 <54591342+luongdoan277@users.noreply.github.com>
2020-06-08 20:45:30 +0700

    add index.php

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from './Components/Navbar'
+import Landing from './Components/Landing'
+import Login from './Components/Login'
+import Register from './Components/Register'
+import Profile from './Components/Profile'
+
+class App extends Component {
+  render() {
+    return (
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/profile" component={Profile} />
+            </div>
+          </div>
+        </Router>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/Components/UserFunctions.js
@@ -0,0 +1,49 @@
+import axios from 'axios'
+
+export const register = User => {
+    return axios
+        .post('http://localhost:8000/api/register', User, {
+            headers: { 'Content-Type': 'application/json' }
+        })
+        .then(response => {
+            console.log(response)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
+
+export const login = user => {
+    return axios
+        .post(
+            'http://localhost:8000/api/login',
+            {
+                email: user.email,
+                password: user.password
+            },
+            {
+                headers: { 'Content-Type': 'application/json' }
+            }
+        )
+        .then(response => {
+            localStorage.setItem('usertoken', response.data.token)
+            return response.data.token
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
+
+export const getProfile = () => {
+    return axios
+        .get('http://localhost:8000/api/profile', {
+            headers: { Authorization: `Bearer ${localStorage.usertoken}` }
+        })
+        .then(response => {
+            console.log(response)
+            return response.data
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
